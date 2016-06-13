e06080d9c28c4a851ca7ffd024b0a1e2d029e33d
Daniel Mikusa <dmikusa@pivotal.io>
2016-06-13 07:53:20 -0400

    Added more tests and flushed out reducer logic.

--- reducers/__tests__/index-test.js
+++ reducers/__tests__/index-test.js
@@ -1,8 +1,8 @@
-jest.unmock('../')
+jest.disableAutomock()
 import calculate from '../'
+import * as actions from '../../actions'
 import * as types from '../../constants/ActionTypes'
 import * as operations from '../../constants/Operations'
-import * as actions from '../../actions'
 
 describe('calculate reducer', () => {
     it('should handle initial state', () => {
@@ -13,7 +13,7 @@ describe('calculate reducer', () => {
             sign: operations.CLEAR
         });
     });
-    if('should handle first button pressed number', () => {
+    it('should handle first button pressed number', () => {
         expect(calculate(undefined, actions.pickNumber(5))).toEqual({
             buffer: 0,
             active: 5,
@@ -21,7 +21,7 @@ describe('calculate reducer', () => {
             sign: operations.CLEAR
         });
     });
-    if('should handle second button pressed number', () => {
+    it('should handle second button pressed number', () => {
         expect(calculate({
             buffer: 0,
             active: 5,
@@ -34,7 +34,7 @@ describe('calculate reducer', () => {
             sign: operations.CLEAR
         });
     });
-    if('should handle third button pressed number', () => {
+    it('should handle third button pressed number', () => {
         expect(calculate({
             buffer: 0,
             active: 57,
@@ -45,6 +45,52 @@ describe('calculate reducer', () => {
             active: 572,
             lastAction: types.PICK_NUMBER,
             sign: operations.CLEAR
+        });
+    });
+    it('should handle button press after operation picked', () => {
+        expect(calculate({
+            buffer: 1,
+            active: 1,
+            lastAction: types.OPERATE,
+            sign: operations.ADD
+        }, actions.pickNumber(2))).toEqual({
+            buffer: 1,
+            active: 2,
+            lastAction: types.PICK_NUMBER,
+            sign: operations.ADD
+        });
+    });
+    it('should handle sign pressed with initial state', () => {
+        expect(calculate(undefined, actions.operate(operations.SUBTRACT))).toEqual({
+            buffer: 0,
+            active: 0,
+            lastAction: types.OPERATE,
+            sign: operations.SUBTRACT
+        });
+    });
+    it('should handle sign pressed when a number is picked', () => {
+        expect(calculate({
+            buffer: 0,
+            active: 57,
+            lastAction: types.PICK_NUMBER,
+            sign: operations.CLEAR
+        }, actions.operate(operations.SUBTRACT))).toEqual({
+            buffer: 57,
+            active: 57,
+            lastAction: types.OPERATE,
+            sign: operations.SUBTRACT
+        });
+    });
+    it('should calculate 1 + 2 = 3', () => {
+        var state = calculate(undefined, actions.pickNumber(1));
+        state = calculate(state, actions.operate(operations.ADD));
+        state = calculate(state, actions.pickNumber(2));
+        state = calculate(state, actions.operate(operations.EQUALS));
+        expect(state).toEqual({
+            buffer: 0,
+            active: 3,
+            lastAction: types.OPERATE,
+            sign: operations.EQUALS
         });
     });
 });
--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,6 @@
 import * as types from '../constants/ActionTypes'
 import * as operations from '../constants/Operations'
+import * as funcs from '../functions/Operations'
 
 const initialState = {
     buffer: 0,
@@ -9,16 +10,32 @@ const initialState = {
 }
 
 export default function calculate(state = initialState, action) {
+    let newState = {};
     switch (action.type) {
         case types.OPERATE:
-            return state;           
+            newState = Object.assign({}, state, {
+                buffer: state.active,
+                sign: action.operation,
+                lastAction: action.type
+            });
+            const op = (action.operation in funcs) ? action.operation : state.sign;
+            if (op in funcs) {
+                if (state.lastAction != types.PICK_NUMBER || action.operation == operations.EQUALS) {
+                    newState.active = funcs[op](state.buffer, state.active);
+                    newState.buffer = 0;
+                }
+                newState.lastAction = action.type;
+                newState.sign = action.operation;
+            }
+            return newState;
         case types.PICK_NUMBER:
             newState = Object.assign({}, state);
-            if (lastAction == types.PICK_NUMBER) {
+            if (state.lastAction == types.PICK_NUMBER) {
                 newState.active = state.active * 10 + action.number;
             } else {
                 newState.active = action.number;
             }
+            newState.lastAction = action.type;
             return newState;
         default:
             return state;
