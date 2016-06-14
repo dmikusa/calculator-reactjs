5408e94190e761041f60cb0320aed578bf0e62c8
Daniel Mikusa <dmikusa@pivotal.io>
2016-06-13 23:55:38 -0400

    Wired up a basic react.js app w/bootswatch

---
+++ components/Calculator.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'
+
+var Calculator = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+});
+
+export default Calculator
