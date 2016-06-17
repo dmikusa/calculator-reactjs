cbe33977dc3c24979f989016e6b4e096570dcd2b
Daniel Mikusa <dmikusa@pivotal.io>
2016-06-16 23:56:19 -0400

    Added basic button component.  Updated Calculator button to display an unformatted calculator keyboard and map handlers to the button.

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,10 +1,50 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import Button from './Button'
 
 class Calculator extends Component {
+    
+    handleNumber(e) {
+        console.log("number: " + e.target.textContent);
+    }
+
+    handleSign(e) {
+        console.log("sign: " + e.target.textContent);
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Calculator</h1>
+                <input type="text" defaultValue="0" />
+                <div>
+                    <Button onClick={this.handleSign} text="AC"/> 
+                    <Button onClick={this.handleSign} text="+/-"/> 
+                    <Button onClick={this.handleSign} text="%"/> 
+                    <Button onClick={this.handleSign} text="&divide;"/> 
+                </div>
+                <div>
+                    <Button onClick={this.handleNumber} text="7"/> 
+                    <Button onClick={this.handleNumber} text="8"/> 
+                    <Button onClick={this.handleNumber} text="9"/> 
+                    <Button onClick={this.handleSign} text="&times;"/> 
+                </div>
+                <div>
+                    <Button onClick={this.handleNumber} text="4"/> 
+                    <Button onClick={this.handleNumber} text="5"/> 
+                    <Button onClick={this.handleNumber} text="6"/> 
+                    <Button onClick={this.handleSign} text="-"/> 
+                </div>
+                <div>
+                    <Button onClick={this.handleNumber} text="1"/> 
+                    <Button onClick={this.handleNumber} text="2"/> 
+                    <Button onClick={this.handleNumber} text="3"/> 
+                    <Button onClick={this.handleSign} text="+"/> 
+                </div>
+                <div>
+                    <Button onClick={this.handleNumber} text="0"/> 
+                    <Button onClick={this.handleNumber} text="."/> 
+                    <Button onClick={this.handleNumber} text="="/> 
+                </div>
             </div>
         );
     }
